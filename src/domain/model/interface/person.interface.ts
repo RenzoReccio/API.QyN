a156f32eb09e36af4318683f234d91fe67f15c7d
Renzo Reccio <43458658+RenzoReccio@users.noreply.github.com>
2022-09-29 00:24:40 +0000

    data model

---
+++ src/domain/model/interface/person.interface.ts
@@ -0,0 +1,7 @@
+export interface Person {
+  id: number;
+  firstName: string;
+  lastName: string;
+  surName: string;
+  bornDate: Date;
+}
---
+++ src/domain/model/interface/purchaseOrderStatus.interface.ts
@@ -0,0 +1,4 @@
+export interface PurchaseOrderStatus {
+  id: number;
+  name: string;
+}
