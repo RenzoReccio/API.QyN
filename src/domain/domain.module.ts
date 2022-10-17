fdb3a141aa561f169aad3a9f2865befb426e5aa8
Renzo Reccio <reccio_2001@hotmail.com>
2022-10-17 08:50:49 -0500

    Fix

--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -2,7 +2,7 @@ import { Module, Provider } from '@nestjs/common';
 import { DataModule } from 'src/data/data.module';
 import { TypeormModule } from 'src/data/typeorm/typeorm.module';
 import { UtilsModule } from 'src/utils/utils.module';
-import { CreateOrderUseCase } from './usecase/order/createOrder/createOrder.usecase';
+// import { CreateOrderUseCase } from './usecase/order/createOrder/createOrder.usecase';
 import { ListOrdersUseCase } from './usecase/order/listOrders/listOrders.usecase';
 import { UpdateOrderUseCase } from './usecase/order/updateOrder/updateOrder.usecase';
 import { ListCatalogUseCase } from './usecase/product/listCatalog/listCatalog.usecase';
@@ -12,9 +12,15 @@ const typeOrmProviders = (): Provider[] => {
   return Reflect.getMetadata('providers', TypeormModule).map((item: Function) => { return { provide: item.name.replace('Service', 'Repository'), useClass: item } })
 }
 
-const OrderUseCases = [CreateOrderUseCase, ListOrdersUseCase, UpdateOrderUseCase]
+const OrderUseCases = [
+  // CreateOrderUseCase, 
+  ListOrdersUseCase, 
+  UpdateOrderUseCase
+]
 
-const ProductUseCases = [ListCatalogUseCase]
+const ProductUseCases = [
+  ListCatalogUseCase
+]
 
 
 @Module({
