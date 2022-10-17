51fc9d91a490d2088ba7eba22322f63180efdd6a
Renzo Reccio <reccio_2001@hotmail.com>
2022-10-17 12:39:40 -0500

    validators

--- src/domain/usecase/auth/login/login.dto.ts
+++ src/domain/usecase/auth/login/login.dto.ts
@@ -1,4 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
 export class LoginDto {
+  @ApiProperty()
+  @IsString({
+    message: 'El nombre de usuario no tiene el formato correcto'
+  })
   userName: string;
+
+  @ApiProperty()
+  @IsString({
+    message: 'La contraseña tiene el formato correcto'
+  })
   password: string;
 }
