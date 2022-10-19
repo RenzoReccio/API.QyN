13e348fdaf9adbb59fb2e830f4f082324dec6dec
Renzo Reccio <43458658+RenzoReccio@users.noreply.github.com>
2022-10-18 20:19:22 -0500

    Update vehicle.service.ts

--- src/data/typeorm/service/vehicle.service.ts
+++ src/data/typeorm/service/vehicle.service.ts
@@ -12,7 +12,7 @@ export class VehicleService implements VehicleRepository {
     return await VehicleEntity.find<VehicleEntity>({ relations: relations ?? [] });
   }
 
-  async findOne(id: number, relations?: string[]): Promise<Vehicle[]> {
+  async findOne(id: number, relations?: string[]): Promise<Vehicle> {
     return await VehicleEntity.findOne<VehicleEntity>({ relations: relations ?? [], where: { id: id } });
   }
 }
